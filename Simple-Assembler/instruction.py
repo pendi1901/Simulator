7d65f2db6612c8200879a83bc09960fdae855381
pendi1901 <79691943+pendi1901@users.noreply.github.com>
2021-08-09 21:44:58 +0300

    update intructions

--- Simple-Assembler/instruction.py
+++ Simple-Assembler/instruction.py
@@ -8,9 +8,9 @@
 #addr_and_pc[1] -> pc
 
 
-def isRegister(instn):
-	#register validation and return boolean
-	#create register dict
+# def isRegister(instn):
+# 	#register validation and return boolean
+# 	#create register dict
 
 def isMem(instn):
 	#memory validation and return boolean
@@ -25,13 +25,19 @@ def isHalt(instn):
 def get_opcode(command):
 	#create dict , format -> {instn: opcode}
 	#return corresponding opcode
-	opcode = {"add": 0000, .....}
+	opcode = {"add": "00000", "sub" : "00001" , "movI" : "00010" , "movR" : "00011",
+	"ld" : "00100" , "st" : "00101" , "mul": "00110" , "div" : "00111" ,
+	"rs" : "01000" , "ls" : "01001" , "xor":"01010" ,"or" : "01011" ,
+	"and" : "01100", "not" : "01101" ,"cmp" : "01110", "jmp":"01111" , 
+	"jlt" : "10000" , "jgt": "10001" , "je" : "10010" , "hlt" : "10011"}
 	return str(opcode[command])
 
 
-def get_regAddr():
+def get_regAddr(reg1):
 	#create reg dict, format -> {reg: reg_Addr}
 	#return corresponding reg addr
+	regdict = {"R0" : "000" , "R1" : "001" , "R2" : "010" , "R3" : "011" , "R4" : "100",
+	"R5" : "101" , "R6" : "110" , "FLAGS" : "111"}
 
 
 
@@ -39,11 +45,15 @@ def get_regAddr():
 #Return incremented mem_addr and machine code if respective validations are correct.
 
 def typeA(instn, machine_code, addr_and_pc):
-	if isRegister(instn):
-		#TRUE: call correponding function
-		op = get_opcode(instn[0]);
-		machine_code.append(op+ 3 registers addr)
-		#FALSE: error
+	# if get_regAddr(instn):
+	# 	#TRUE: call correponding function
+	# 	op = get_opcode(instn[0])
+	# 	redudant = "00"
+
+	# 	machine_code.append(op+redudant + )
+	# 	#FALSE: error
+	if(len(instn) == 4):
+		
 
 def typeB(instn, machine_code, addr_and_pc):
 	if isRegister(instn):
@@ -70,17 +80,24 @@ def typeE(instn, machine_code, addr_and_pc):
 		#FALSE: error
 
 def typeF(instn, machine_code, addr_and_pc):
-	if isHalt(instn)
+	if isHalt(instn):
 		#TRUE: call correponding function
 		#FALSE: error
 
 def instruction(instn, machine_code, addr_and_pc):
 	#like A -> all type A instructions -> "add", "sub", "mul" etc.
-	instn_type = { A: [instn of A ], B: [instn of B] , so on}
+	index = instn[2].find('$')
+	if(index == 0):
+		instn[0] = "movI"
+	else:
+		instn[0]="movR"
+	instn_type = { "A": ["add" , "sub" , "mul" , "xor" , "or" , "and" ],
+	"B" : ["movI" , "rs","ls"], "C" : ["movR", "div" ,"not" , "cmp"] , "D" : ["ld" , "st"],
+	"E" : ["jmp" , "jlp" , "jgt" , "je"] , "F" :["hlt"]}
 	for i in instn_type:
-		if instn[0] in instn_type[A]:  typeA(instn, machine_code, addr_and_pc)
-		elif instn[0] in instn_type[B]:  typeB(instn, machine_code, addr_and_pc)
-		elif instn[0] in instn_type[C]:  typeC(instn, machine_code, addr_and_pc)
-		elif instn[0] in instn_type[D]:  typeD(instn, machine_code, addr_and_pc)
-		elif instn[0] in instn_type[E]:  typeE(instn, machine_code, addr_and_pc)
-		else typeF(instn, machine_code, addr_and_pc)
+		if instn[0] in instn_type["A"]:  typeA(instn, machine_code, addr_and_pc)
+		elif instn[0] in instn_type["B"]:  typeB(instn, machine_code, addr_and_pc)
+		elif instn[0] in instn_type["C"]:  typeC(instn, machine_code, addr_and_pc)
+		elif instn[0] in instn_type["D"]:  typeD(instn, machine_code, addr_and_pc)
+		elif instn[0] in instn_type["E"]:  typeE(instn, machine_code, addr_and_pc)
+		else :  typeF(instn, machine_code, addr_and_pc)
