ab1589b97223fc841ca7f129c5aa95941e5a879c
alvinjoseph7 <alvin9joseph@gmail.com>
2021-08-11 13:33:18 +0530

    changed typeF fn

--- Simple-Assembler/instruction.py
+++ Simple-Assembler/instruction.py
@@ -10,23 +10,15 @@
 
 def isMem(instn):
 	#memory validation and return boolean
-	if(instn<=256):
-		return True
-	else:
-		return False
+	#to be checked.................
+	return instn < 256
 
 def isImm(instn):
 	#immediate validation and return boolean
-	if(instn<=256):
-		return True
-	else:
-		return False
+	return instn < 256
 
 def isHalt(instn):
-	if(instn[0]== "hlt"):
-		return True
-	else : 
-		return False
+	return len(instn) == 1
 
 def get_opcode(command):
 	#create dict , format -> {instn: opcode}
@@ -42,8 +34,8 @@ def get_opcode(command):
 def get_regAddr(reg1):
 	#create reg dict, format -> {reg: reg_Addr}
 	#return corresponding reg addr
-	regdict = {"R0" : "000" , "R1" : "001" , "R2" : "010" , "R3" : "011" , "R4" : "100",
-	"R5" : "101" , "R6" : "110" , "FLAGS" : "111"}
+	regdict = {"R0" : "000" , "R1" : "001" , "R2" : "010" , "R3" : "011" ,
+	 			"R4" : "100", "R5" : "101" , "R6" : "110" , "FLAGS" : "111"}
 	if(reg1 in regdict):
 		return str(regdict[reg1])
 	else : 
@@ -62,18 +54,19 @@ def typeA(instn, machine_code, addr_and_pc):
 	if(len(instn) != 4):
 		raise Exception("Error : Not Enough Arguments","At line number", addr_and_pc[1])
 	else:
-		a = get_regAddr(instn[1])
-		b = get_regAddr(instn[2])
-		c = get_regAddr(instn[3])
-		op = get_opcode(instn[0])
-		redundant = "00"
-		machine_code.append(op + redundant + a + b + c )
-		addr_and_pc[0]+=1
+		rd = get_regAddr(instn[1])
+		rs1 = get_regAddr(instn[2])
+		rs2 = get_regAddr(instn[3])
+		opcode = get_opcode(instn[0])
+		unused = "00"
+		machine_code.append(opcode + unused + rd + rs1 + rs2 )
+		
+	return (addr_and_pc[0] + 1)
 		
 
 def typeB(instn, machine_code, addr_and_pc):
 	b = isImm(instn[2][1:])
-	if(len(instn)==3):
+	if(len(instn) == 3):
 		if(b == True):
 			a = get_regAddr(instn[1])
 			op = get_opcode(instn[0])
@@ -130,15 +123,18 @@ def typeE(instn, machine_code, addr_and_pc):
 
 #complete this function to check the if halt is the last statement
 def typeF(instn, machine_code, addr_and_pc):
-	a = isHalt(instn[0])
-	if(len(instn)==1):
-		if(a == True ):
-			addr_and_pc[2]+=1
-			if(addr_and_pc[2] == 1):
-				op =get_opcode(instn[0])
-				redundant = "00000000000"
-				machine_code.append(op+redundant)
-				addr_and_pc[0]+=1
+	# no point checking this. Checked in main itself
+	#a = isHalt(instn[0])
+	if(isHalt(instn)):
+		# not returning val of hlt_count, so cannot keep track
+		opcode =get_opcode(instn[0])
+		unused = "00000000000"
+		machine_code.append(opcode + unused)
+	
+	else:	
+		raise #Incorrect no of parameters given or Synatx error
+			
+	return addr_and_pc[0] + 1
 				
 			
 
@@ -147,14 +143,20 @@ def typeF(instn, machine_code, addr_and_pc):
 
 def instruction(instn, machine_code, addr_and_pc):
 	#like A -> all type A instructions -> "add", "sub", "mul" etc.
-	index = instn[2].find('$')
-	if(index == 0):
-		instn[0] = "movI"
-	else:
-		instn[0]="movR"
+	if instn[0] == "mov":
+		index = instn[2].find('$')
+		if(index == 0):
+			instn[0] = "movI"
+		else:
+			instn[0]="movR"
+
 	instn_type = { "A": ["add" , "sub" , "mul" , "xor" , "or" , "and" ],
-	"B" : ["movI" , "rs","ls"], "C" : ["movR", "div" ,"not" , "cmp"] , "D" : ["ld" , "st"],
-	"E" : ["jmp" , "jlp" , "jgt" , "je"] , "F" :["hlt"]}
+				   "B" : ["movI" , "rs","ls"], 
+				   "C" : ["movR", "div" ,"not" , "cmp"] , 
+				   "D" : ["ld" , "st"],
+				   "E" : ["jmp" , "jlp" , "jgt" , "je"] ,
+				   "F" :["hlt"]
+				 }
 	for i in instn_type:
 		if instn[0] in instn_type["A"]: 
 			 typeA(instn, machine_code, addr_and_pc)
